cc9e2241249267e616903b71ecbfa1440abc4829
jniestroy <jniestroy@gmail.com>
2019-08-22 17:45:59 -0400

    updated wf

--- Figure WF Files/create_fig.py
+++ Figure WF Files/create_fig.py
@@ -14,8 +14,8 @@ def data_cleaning(clean):
 
 
     ax1 = plt.subplot(131)
-    ax1.hist(fig_df['Pulse'], np.linspace(30, 150, 100), alpha=0.5, label='Charted VS')
-    ax1.hist(fig_df['hr'], np.linspace(30, 150, 100), alpha=0.5, label='ECG',color = "red")
+    ax1.hist(fig_df['Pulse'], np.linspace(30, 150, 120), alpha=0.5, label='Charted VS')
+    ax1.hist(fig_df['hr'], np.linspace(30, 150, 120), alpha=0.5, label='ECG',color = "red")
     ax1.set_title('Heart Rate')
     ax1.set_xlabel("Beats/Min")
     ax1.set_ylabel('Count')
@@ -24,7 +24,7 @@ def data_cleaning(clean):
 
     ax2 = plt.subplot(132)
     ax2.hist(fig_df['Resp'], np.linspace(10, 40, 30), alpha=0.5, label='Charted VS')
-    ax2.hist(fig_df['edrk'], np.linspace(10, 40, 30), alpha=0.5, label='ECG',color = "red")
+    ax2.hist(fig_df['edrk'], np.linspace(10, 40, 30), alpha=0.5, label='ECG VS',color = "red")
     ax2.set_xlabel('Breaths/Min')
     ax2.set_title('Resp Rate')
     ax2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
